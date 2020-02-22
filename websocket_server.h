d8da2d092520ecc6f5e5941a6422a96bb55904b3
anil235476 <aks.dce.007@gmail.com>
2020-02-22 19:21:15 +0530

    adding refactoring changes

--- websocket_server.h
+++ websocket_server.h
@@ -1,6 +1,6 @@
 #ifndef _WEBSOCKET_SERVER_H__
 #define _WEBSOCKET_SERVER_H__
-#include "function_thread_handler/func_thread_handler.h"
+#include "func_thread_handler.h"
 namespace grt {
 	void 
 	start_server_block(unsigned short port, int threads, util::func_thread_handler* caller,
